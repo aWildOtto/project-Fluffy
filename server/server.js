73e425dedda61046ab22dd86e9ff0abecea9c3d3
Otto Hu <ottohu101@gmail.com>
2018-01-22 22:58:48 -0800

    project fresh setup

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const ENV = process.env.ENV || "development";
+
+const http = require('http');
+const express = require("express");
+const app = express();
+const server = http.createServer(app);
+const path = require('path');
+
+const knexConfig = require("./knexfile");
+const knexLogger = require('knex-logger');
+const knex = require("knex")(knexConfig[ENV]);
+app.use(knexLogger(knex));
+
+const bodyParser = require("body-parser");
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get("*", (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('Server running at localhost:' + process.env.PORT || 3000);
+});
