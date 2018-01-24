f5fbef56c90f8eb5936ea2edf953361c5b3698b0
Otto Hu <ottohu101@gmail.com>
2018-01-23 23:37:05 -0800

    setup front end and back end

--- fluffy/src/app/app.module.ts
+++ fluffy/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
--- server/db/migrations/20180122224148_first_migration.js
+++ server/db/migrations/20180122224148_first_migration.js
@@ -1,8 +1,20 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+    return Promise.all([
+        knex.schema.createTable('users', (table) => {
+            table.increments('id').primary;
+            table.string('first_name');
+            table.string('last_name');
+            table.string('email');
+            table.string('password');
+            table.timestamp('created_at').defaultTo(knex.fn.now());
+            table.timestamp('edited_at');
+        })
+    ])
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+    return Promise.all([
+        knex.schema.dropTable('users')
+    ])
 };
